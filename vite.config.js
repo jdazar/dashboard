86d461d41b6db825e136cd76e35af92673af565f
Julian Daza <jdazar@unal.edu.co>
2025-02-10 10:59:11 -0500

    feat: changed github username in links

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://jdazar18.github.io/dashboard"
+  base:"https://jdazar.github.io/dashboard"
   
 })
